28c5791061f3dfe9e1d90d8df7cb1504f53781c6
Mohamed Harby <71990569+Mohamed-Harby@users.noreply.github.com>
2022-05-05 17:13:56 +0200

    Update single-server-system.cpp

--- single-server-system.cpp
+++ single-server-system.cpp
@@ -1,18 +1,11 @@
 #include <bits/stdc++.h>
 
 #define ll long long
-#define INF INT_MAX
 #define IDLE 0
 #define ACTIVE 1
 using namespace std;
+double INF = 1e9;
 
-void init() {
-//    ios_base::sync_with_stdio(false);
-//    cin.tie(nullptr);
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-#endif
-}
 
 class Process {
 public:
@@ -30,7 +23,7 @@ public:
 
 class Clock {
 public:
-    int time;
+    double time;
     Clock(int t) : time(t) {};
     Clock() {
         time = 0;
@@ -46,15 +39,6 @@ public:
     }
 };
 
-Clock clk, prev_clk;
-vector<Process> processes;
-// ----------------Statical variables
-int cnt_delayed, total_delay_time;
-double area_under_q, area_under_b; // (clock value - time of last event) * height
-// arrival time, and service time
-int process_index = 0;
-bool finished;
-int curr_time;
 
 class Event {
 public:
@@ -63,73 +47,90 @@ public:
     Event() {
         value = INF;
     }
-    void update_departure() {
-        value = clk.time + processes[process_index].service_time;
+    bool operator==(Event &ev) {
+        return this->value == ev.value;
     }
-    void update_arrival() {
-        value = processes[process_index].arrival_time;
+    bool operator==(double v) {
+        return this->value == v;
     }
 };
 
-Event next_arrival_time; // A
-Event current_departure_time; // D
-// ----------------System state variables
-Server server_1;
-queue<Process> waiting_queue;
-// ----------------event list
-
-
-
-void get_time(int &curr_time) {
-    if (current_departure_time.value == INF)
-        curr_time = next_arrival_time.value;
-    if (next_arrival_time.value < current_departure_time.value)
-        curr_time = next_arrival_time.value;
-    else
-        curr_time = current_departure_time.value;
-}
-
-void update_clocks(double next_clk) {
-    prev_clk.time = clk.time;
-    clk.time = next_clk;
-}
-
-void update_graph() {
-    area_under_q += (clk.time - prev_clk.time) * (int) waiting_queue.size();
-    area_under_b += clk.time - prev_clk.time;
+Event min_event(vector<Event> events) {
+    Event minimum = events[0];
+    for (Event e: events) {
+        if (e.value < minimum.value)
+            minimum = e;
+    }
+    return minimum;
 }
 
 int main() {
-    init();
+
+    /* ------- Given system data -------- */
+
     vector<double> arrival_time = {0, 5, 3, 1, 1};
     vector<double> service_time = {3, 2, 1, 2, 1};
+
+//    vector<double> arrival_time = {0, 2, 4, 4, 2, 2};
+//    vector<double> service_time = {5, 3, 3, 5, 6, 3};
+
+    /* ------- System components -------- */
+
+    // Data
+    vector<Process> processes;
+    vector<Process>::iterator current_process;
+
+    // Components
+    Server server_1;
+    queue<Process> waiting_queue;
+    Event next_arrival_time, current_departure_time;
+    Event curr_event;
+
+    // Clocks
+    Clock clk, prev_clk;
+
+    // Statics
+    double area_under_q = 0, area_under_b = 0;
+    double total_delay_time = 0;
+    int delayed_processes = 0;
+
+
+    /* ------- Preprocessing -------- */
+
     processes.clear();
     vector<double> arrival(arrival_time.size());
-    for (int i = 1; i < arrival_time.size(); ++i)
+    for (int i = 1; i < arrival_time.size(); ++i) {
         arrival[i] = arrival[i - 1] + arrival_time[i];
-    for (int i = 0; i < arrival_time.size(); ++i) {
-        processes.push_back({arrival_time[i], service_time[i], arrival[i]});
     }
-//    processes.push_back({INF, INF, INF});
+    for (int i = 0; i < arrival_time.size(); ++i) {
+        processes.emplace_back(arrival_time[i], service_time[i], arrival[i]);
+    }
+    // todo: add to system components
+    next_arrival_time.value = processes[0].interarrival_time;
+    current_process = processes.begin();
 
-    next_arrival_time = processes[0].interarrival_time;
-    cnt_delayed = 0, total_delay_time = 0, area_under_q = 0, area_under_b = 0, process_index = 0;
-    curr_time = 0;
-    int delayed_processes = 0;
+    /* ------- Simulation -------- */
+
     while (true) {
-        get_time(curr_time);
-        if (curr_time == INF)
+        curr_event = min_event({current_departure_time, next_arrival_time});
+        if (curr_event == INF)
             break;
-        update_clocks(curr_time);
-        if (curr_time == current_departure_time.value) { // curr_time == current_departure_time
-            update_graph();
+
+        // update clocks
+        prev_clk.time = clk.time;
+        clk.time = curr_event.value;
+
+        if (curr_event == current_departure_time) {
+            // update statistics
+            area_under_q += (clk.time - prev_clk.time) * (int) waiting_queue.size();
+            area_under_b += clk.time - prev_clk.time;
+
             if (!waiting_queue.empty()) { // queue is not empty
-                Process curr_process = waiting_queue.front();
+                Process next_process = waiting_queue.front();
                 waiting_queue.pop();
-                total_delay_time += clk.time - curr_process.arrival_time;
+                total_delay_time += clk.time - next_process.arrival_time;
                 current_departure_time.value =
-                        clk.time + curr_process.service_time; // add waiting time of the top process
-                cnt_delayed++;
+                        clk.time + next_process.service_time; // add waiting time of the top process
             } else { // no items to insert
                 server_1.status = IDLE;
                 current_departure_time.value = INF;
@@ -137,27 +138,47 @@ int main() {
         } else { // curr_time == arrival_time
             if (server_1.status == IDLE) { // server is idle
                 server_1.status = ACTIVE;
-                current_departure_time.update_departure();
-                cnt_delayed++;
+                // update_departure for the current process in the server
+                current_departure_time.value = clk.time + (*current_process).service_time;
+
             } else { // server is active
-                update_graph();
-                waiting_queue.push(processes[process_index]);
+                // update statistics
+                area_under_q += (clk.time - prev_clk.time) * (int) waiting_queue.size();
+                area_under_b += clk.time - prev_clk.time;
+
+                waiting_queue.push(*current_process);
                 delayed_processes++;
             }
-            if (process_index == processes.size() - 1) {
+            if (current_process == processes.end() - 1) {
                 next_arrival_time.value = INF;
                 continue;
             }
-            process_index++;
-            next_arrival_time.update_arrival();
+            current_process++;
+            // update_arrival to the next process
+            next_arrival_time.value = (*current_process).arrival_time;
         }
     }
 
-    cerr << "clock value: " << clk.time << '\n';
-    cerr << "total_delay_time value: " << total_delay_time << '\n';
-    cerr << "Average waiting time: " << total_delay_time / processes.size() << '\n';
-    cout << "Average delay time: " << total_delay_time * 1.0 / delayed_processes << '\n';
-    cout << "Average number of processes in queue: " << area_under_q * 1.0 / clk.time << '\n';
-    cout << "Area under b: " << area_under_b << '\n' << "clock final value: " << clk.time << '\n';
-    cout << "utilization: " << area_under_b * 1.0 / clk.time << '\n';
+
+    /* ------- Output statistics -------- */
+
+    cout << "The Average waiting time of those who wait in queue d(n) = "
+         << total_delay_time * 1.0 / delayed_processes << " min\n";
+
+    cout << "Time-average number in queue q(n) = " << area_under_q * 1.0 / clk.time << " process\n";
+
+    cout << "Total busy time B(t) = " << area_under_b << " min\n";
+
+    cout << "Utilization u(n) = " << area_under_b * 1.0 / clk.time << '\n';
+
+    cout << "Average service time = " << area_under_b * 1.0 / double(processes.size()) << " min\n";
+
+    cout << "Average waiting time = " << total_delay_time / double(processes.size()) << " min\n";
+
+    cout << "Average time customer spends in the system = "
+         << (total_delay_time + area_under_b) / double(processes.size()) << " min\n";
+
+    cout << "Throughput = " << double(processes.size()) / clk.time << " process/min\n";
+
+
 }
